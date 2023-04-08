ca5a770cc52bee91c4700b6b602280d3e044b5af
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-04-08 13:57:05 -0400

    Added  JS formula on how to calculate the amount  per person

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const calculateButton = document.querySelector("#calculateBtn");
 calculateButton.addEventListener("click", calculateAmount);
 
-function calculateAmount(){
+function calculateAmount(event){
+	event.preventDefault();
 	const billInput = document.querySelector("#bill").value;
 	const numberOfPeopleInput = document.querySelector("#people").value;
-	const tipInput = document.querySelector("#addTip").value;
+	const tipInput = document.querySelector("#tip").value;
+
+	if (billInput === "" || numberOfPeopleInput === "" ){
+		alert("Error");
+	}
+
+	let amountPerPerson = billInput/numberOfPeopleInput;
+	console.log(amountPerPerson);
+
+	let tipPerPerson = (billInput * tipInput) /numberOfPeopleInput;
+	console.log(tipPerPerson);
+
+	let totalSum = amountPerPerson + tipPerPerson;
+	console.log(totalSum);
+
 }
