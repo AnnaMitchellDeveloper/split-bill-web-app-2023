657f3b07e15fb1f5a158a3342f8e8369aceab4d3
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-04-08 13:19:15 -0400

    Added in JS function for calculate button

---
+++ index.js
@@ -0,0 +1,8 @@
+const calculateButton = document.querySelector("#calculateBtn");
+calculateButton.addEventListener("click", calculateAmount);
+
+function calculateAmount(){
+	const billInput = document.querySelector("#bill").value;
+	const numberOfPeopleInput = document.querySelector("#people").value;
+	const tipInput = document.querySelector("#addTip").value;
+}
