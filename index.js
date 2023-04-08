cffb22747a975c8f5f811bd7ed9564620a665700
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-04-08 14:18:05 -0400

    Added sweet alert

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const calculateButton = document.querySelector("#calculateBtn");
 calculateButton.addEventListener("click", calculateAmount);
 
-function calculateAmount(event){
-	event.preventDefault();
-	const billInput = document.querySelector("#bill").value;
-	const numberOfPeopleInput = document.querySelector("#people").value;
-	const tipInput = document.querySelector("#tip").value;
+function calculateAmount(event) {
+  event.preventDefault();
+  const billInput = document.querySelector("#bill").value;
+  const numberOfPeopleInput = document.querySelector("#people").value;
+  const tipInput = document.querySelector("#tip").value;
 
-	if (billInput === "" || numberOfPeopleInput === "" ){
-		alert("Error");
-	}
+  if (billInput === "" || numberOfPeopleInput === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
+    });
+  }
 
-	let amountPerPerson = billInput/numberOfPeopleInput;
-	console.log(amountPerPerson);
+  let amountPerPerson = billInput / numberOfPeopleInput;
+  let tipPerPerson = (billInput * tipInput) / numberOfPeopleInput;
+  let totalSum = amountPerPerson + tipPerPerson;
 
-	let tipPerPerson = (billInput * tipInput) /numberOfPeopleInput;
-	console.log(tipPerPerson);
+  amountPerPerson = amountPerPerson.toFixed(2);
+  tipPerPerson = tipPerPerson.toFixed(2);
+  totalSum = totalSum.toFixed(2);
 
-	let totalSum = amountPerPerson + tipPerPerson;
-	console.log(totalSum);
-
+  document.querySelector("#dividedBill").textContent = amountPerPerson;
+  document.querySelector("#dividedTip").textContent = tipPerPerson;
+  document.querySelector("#billAndTip").textContent = totalSum;
 }
